719e6cdf9676427d97d6052d8d40d0b1fe7d9c75
mahesh chomal <maheshchomal@caperminttechnologies.com>
2022-02-28 19:12:22 +0530

    update

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,16 +3,16 @@ const controller = require("../controllers/auth.controller");
 const multipart = require('connect-multiparty');
 const multipartMiddleware = multipart();
 const validationMiddleware = require('../middleware/validation-middleware');
-const DIR = './public/avatar/';
+
 const multer = require('multer');
 const path = require("path");
 //const upload = multer({dest:DIR}).single("avatar");
 //const SpinnControlers = require("../controllers/spinn.controller");
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, DIR);
-  },
+    destination: function (req, file, cb) {
+      cb(null, DIR);
+    },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
   },
@@ -24,11 +24,13 @@ var upload = multer({
   fileFilter: (req, file, cb) => {
     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg"
     ) { 
+      // To accept the file pass `true`, like so:
       cb(null, true);
     } else {
+      // To reject this file pass `false`, like so:
       cb(null, false);
-      return "Only .png, .jpg and .jpeg format allowed!";
-      //return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+      //return "Only .png, .jpg and .jpeg format allowed!";
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
     }
   },
   limits: { fileSize: maxSize },
@@ -52,7 +54,7 @@ module.exports = function(app) {
     upload,
   ],controller.signup);*/
 
-  app.post('/api/registration',[upload,verifySignUp.checkDuplicateEmail,validationMiddleware.signup],controller.signup);
+  app.post('/api/registration',[validationMiddleware.image_upload,verifySignUp.checkDuplicateEmail,validationMiddleware.signup],controller.signup);
 
 /*  app.post("/api/registration", (req, res) => {
      upload(req, res, (err) => {
